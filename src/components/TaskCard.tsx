e29d8e3ff24d7c1f88b60573f815fdeae87d6207
LeoZion <leozionsuporte@gmail.com>
2022-01-22 20:58:16 -0300

    config drag task

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,16 +1,36 @@
 import { Box, Flex, Text, Badge } from "@chakra-ui/react"
+import { useDrag } from "react-dnd"
+import itemTypes from "../utils/itemTypes"
 
 interface TaskCardProps {
+    id: string;
     title: string;
     category: string;
     details: string;
 }
 
 const TaskCard: React.FC<TaskCardProps> = (props) => {
-    const { title, category, details } = props
+    const { id, title, category, details } = props
+    const [{ isDragging }, drag] = useDrag(() => ({        
+        type: itemTypes.CARD,
+        item: () => {
+            return {id}
+        },
+        collect: (monitor) => ({
+            isDragging: !!monitor.isDragging()
+        })
+    }))
 
     return (
-        <Box bg="teal.200" rounded="md" boxShadow="sm" p={3} mt={4}>
+        <Box
+            ref={drag}
+            opacity={isDragging ? 0.5 : 1}
+            bg="teal.200"
+            rounded="md"
+            boxShadow="sm"
+            p={3}
+            mt={4}
+        >
             <Flex justify="space-between" align="baseline" mb={3}>
                 <Text>{title}</Text>
                 <Badge variant="solid" colorScheme={category === 'chores' ? 'orange' : 'purple'}>{category}</Badge>
