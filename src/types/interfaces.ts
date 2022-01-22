d84b9305810b9cf605c3fadd4d749bcb1ff6b5d9
LeoZion <leozionsuporte@gmail.com>
2022-01-22 20:57:34 -0300

    add itemTypes and interfaces

---
+++ src/types/interfaces.ts
@@ -0,0 +1,12 @@
+export interface TASK {
+    id: string;
+    title: string;
+    status: string;
+    category: string;
+    details: string;
+}
+
+export interface ITEM {
+    type: string;
+    id: string;
+}
